dbd20fd4f1f89917dd1c1aa18d2cdb63ac4d52bc
stanly08 <anasistanly@gmail.com>
2023-07-13 02:27:52 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A code that outputs a sentiment which has puts function
+ * return: 0 (Brilliant)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
